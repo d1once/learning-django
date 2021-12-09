f11ec774b11e9b0e3677805e1809a81b5170a8c1
Dionis <dionisenja@gmail.com>
2021-12-09 14:37:43 +0100

    rumors continue

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,9 +1,10 @@
 from reviews.models import Review
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic import CreateView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -13,29 +14,29 @@ from .models import Review
 
 class ReviewView(CreateView):
     model = Review
-    fields = "__all__"
+    form_class = ReviewForm
     template_name = "reviews/review.html"
     success_url = "/thank-you"
 
 
-class ThankYouView(ListView):
+class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
     model = Review
+    context_object_name = "reviews"
 
     # def get_queryset(self):
     #     base_query = super().get_queryset()
     #     data = base_query.filter(rating__gt=4)
     #     return data
-
-
-class ReviewsListView(TemplateView):
-    template_name = "reviews/review_list.html"
-
-    def get_context_data(self, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        contex["reviews"] = reviews
-        return contex
 
 
 class SingleReviewView(DetailView):
